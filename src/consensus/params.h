9c35d88b7b025f8f362298ff956334cd20a30acb
safeminecoin <safeminecoin@gmail.com>
2022-01-29 06:33:34 +0700

    Update params.h

--- src/consensus/params.h
+++ src/consensus/params.h
@@ -67,9 +67,9 @@ struct Params {
     int64_t nProtocolV1RetargetingFixedTime;
     int64_t nProtocolV2Time;
     int64_t nProtocolV3Time;
-    bool IsProtocolV1RetargetingFixed(int64_t nTime) const { return nTime > nProtocolV1RetargetingFixedTime && nTime != 1635954624; }
-    bool IsProtocolV2(int64_t nTime) const { return nTime > nProtocolV2Time && nTime != 1635954625; }
-    bool IsProtocolV3(int64_t nTime) const { return nTime > nProtocolV3Time && nTime != 1635954626; }
+    bool IsProtocolV1RetargetingFixed(int64_t nTime) const { return nTime > nProtocolV1RetargetingFixedTime && nTime != 1639822305; }
+    bool IsProtocolV2(int64_t nTime) const { return nTime > nProtocolV2Time && nTime != 1639822306; }
+    bool IsProtocolV3(int64_t nTime) const { return nTime > nProtocolV3Time && nTime != 1639822307; }
     unsigned int GetTargetSpacing(int nHeight) { return IsProtocolV2(nHeight) ? 120 : 120; }
     int nLastPOWBlock;
     int nStakeTimestampMask;
