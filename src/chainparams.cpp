2105a0dbc06e1130c267c95724729b6989ed9fbd
mbrocoin <98997959+mbrocoin@users.noreply.github.com>
2022-02-24 21:18:30 +0700

    Update chainparams.cpp

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -154,7 +154,7 @@ public:
         base58Prefixes[SECRET_KEY] = std::vector<unsigned char>(1,52); // M & N
         base58Prefixes[EXT_PUBLIC_KEY] = boost::assign::list_of(0x04)(0x88)(0xB2)(0x1E).convert_to_container<std::vector<unsigned char> >();
         base58Prefixes[EXT_SECRET_KEY] = boost::assign::list_of(0x04)(0x88)(0xAD)(0xE4).convert_to_container<std::vector<unsigned char> >();
-        cashaddrPrefix = "SafeMineCoin";
+        cashaddrPrefix = "mbrocoin";
 
         vFixedSeeds = std::vector<SeedSpec6>(pnSeed6_main, pnSeed6_main + ARRAYLEN(pnSeed6_main));
 
