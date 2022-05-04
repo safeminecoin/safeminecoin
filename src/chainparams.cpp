515cdd0fb8516344b8af67cb6d6fdee90fef4585
alfariqicrypto <94574305+safeminecoin@users.noreply.github.com>
2022-05-05 01:11:01 +0700

    checkpoints 153290

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -142,8 +142,8 @@ public:
         checkpointData = (CCheckpointData) {
                     boost::assign::map_list_of
                     (0, uint256S("0x00000515601d3a48a250b62584becbc12fa50fb12609dc81b0732e9a05804d72"))
-                    (123600, uint256S("db7b2e65576491e6dd0d44008870d6252d06c5669dc6ea929dce3c0c755fcad6")),					
-                    1648805367, // * UNIX timestamp of last checkpoint block
+                    (153290, uint256S("0x743f7863e299fb938443f7785250474fc8a9fad5eab574b622283d838fb22469")),					
+                    1651687602, // * UNIX timestamp of last checkpoint block
                     0,    // * total number of transactions between genesis and last checkpoint
                                 //   (the tx=... number in the SetBestChain debug.log lines)
                     0      // * estimated number of transactions per day after checkpoint
